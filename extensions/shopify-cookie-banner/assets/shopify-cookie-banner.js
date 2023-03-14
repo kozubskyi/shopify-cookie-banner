9c5f34a2b56bb794062df26f14b2d93d245f4507
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-03-14 15:30:25 +0200

    update js file

--- extensions/shopify-cookie-banner/assets/shopify-cookie-banner.js
+++ extensions/shopify-cookie-banner/assets/shopify-cookie-banner.js
@@ -1,6 +1,8 @@
 const banner = document.querySelector('.shopify-cookie-banner')
 const bannerButtons = document.querySelector('.shopify-cookie-banner .buttons')
 
-const hideBanner = () => banner.classList.add('hidden')
+const hideBanner = () => banner.remove()
 
-console.dir(bannerButtons.children[0])
+for (let i = 0; i < bannerButtons.children.length; i++) {
+	bannerButtons.children[i].addEventListener('click', hideBanner)
+}
