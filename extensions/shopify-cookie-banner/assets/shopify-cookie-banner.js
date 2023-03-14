953b842556db01d57d43077aa0a17691a07b0596
Kozubskyi Denys <kozubskiy.denis@gmail.com>
2023-03-14 14:41:43 +0200

    first commit

---
+++ extensions/shopify-cookie-banner/assets/shopify-cookie-banner.js
@@ -0,0 +1,6 @@
+const banner = document.querySelector('.shopify-cookie-banner')
+const bannerButtons = document.querySelector('.shopify-cookie-banner .buttons')
+
+const hideBanner = () => banner.classList.add('hidden')
+
+console.dir(bannerButtons.children[0])
